37ccec41da328c3d19384c91fd3b65e26350b8a0
RamonUlises <jorgitomilina@gmail.com>
2025-09-30 14:19:46 -0600

    Agregar archivos de proyecto.

---
+++ kanbarugym/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kanbarugym
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kanbarugym/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "kanbarugym")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "kanbarugym.Pages")]
