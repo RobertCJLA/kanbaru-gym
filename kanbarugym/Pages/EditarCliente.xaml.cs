498e07cb7453fae92f76e7014faec2dd170c7d5f
katsuhira <katsuhira@DESKTOP-ISOLCHI>
2025-11-05 14:09:15 -0600

    editar cliente

--- kanbarugym/Pages/EditarCliente.xaml.cs
+++ kanbarugym/Pages/EditarCliente.xaml.cs
@@ -1,9 +1,25 @@
-namespace kanbarugym.Pages;
+using System;
+using Microsoft.Maui.Controls;
 
-public partial class EditarCliente : ContentPage
+namespace kanbarugym.Pages
 {
-	public EditarCliente(string id, string nombres, string fechaNacimiento, string correoElectronico, string telefono, string sexo)
-	{
-		InitializeComponent();
-	}
+    public partial class EditarCliente : ContentPage
+    {
+        public EditarCliente(string id, string nombres, string fechaNacimiento, string correoElectronico, string telefono, string sexo)
+        {
+            InitializeComponent();
+            txtNombreCliente.Text = nombres;
+            txtFechaNacimiento.Text = fechaNacimiento;
+            txtNECliente.Text = correoElectronico;
+            txtNTClient.Text = telefono;
+            cmbSexo.Title = sexo;
+        }
+
+        // Método controlador de eventos para el botón "Guardar"
+        private void OnEditClient(object sender, EventArgs e)
+        {
+            // Aquí puedes agregar la lógica para editar el cliente
+            // Por ejemplo, validar y guardar los datos ingresados
+        }
+    }
 }
